40910f54155a2adfe9afe7f0871ea506e7a6d858
Static-Flow <tanner989@gmail.com>
2020-12-28 23:17:11 -0600

    removed header entropy level config from UI

--- src/requestcleaner/BurpCleanerPanel.java
+++ src/requestcleaner/BurpCleanerPanel.java
@@ -24,11 +24,6 @@ public class BurpCleanerPanel extends JPanel {
         ExtensionState.getInstance().setNumberOfCleanedRequests(ExtensionState.getInstance().getNumberOfCleanedRequests()+1);
     }
 
-
-    private void headerSetLevelActionPerformed(ActionEvent e) {
-        ExtensionState.getInstance().setHeaderEntropyLevel(Integer.parseInt(headerLevelText.getText()));
-    }
-
     private void parameterSetLevelActionPerformed(ActionEvent e) {
         ExtensionState.getInstance().setParameterEntropyLevel(Integer.parseInt(parameterLevelText.getText()));
     }
@@ -38,39 +33,22 @@ public class BurpCleanerPanel extends JPanel {
         // Generated using JFormDesigner Evaluation license - unknown
         cleanerTabPane = new JTabbedPane();
         panel1 = new JPanel();
-        label1 = new JLabel();
-        headerLevelText = new JTextField();
-        headerSetLevel = new JButton();
         label2 = new JLabel();
         parameterLevelText = new JTextField();
         parameterSetLevel = new JButton();
 
         //======== this ========
-        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing.
-        border. EmptyBorder( 0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax. swing. border. TitledBorder. CENTER
-        , javax. swing. border. TitledBorder. BOTTOM, new java .awt .Font ("Dia\u006cog" ,java .awt .Font
-        .BOLD ,12 ), java. awt. Color. red) , getBorder( )) );  addPropertyChangeListener (
-        new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062ord\u0065r"
-        .equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
+        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(
+        0,0,0,0), "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e",javax.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder
+        .BOTTOM,new java.awt.Font("D\u0069al\u006fg",java.awt.Font.BOLD,12),java.awt.Color.
+        red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.
+        beans.PropertyChangeEvent e){if("\u0062or\u0064er".equals(e.getPropertyName()))throw new RuntimeException();}});
         setLayout(new BorderLayout());
         add(cleanerTabPane, BorderLayout.CENTER);
 
         //======== panel1 ========
         {
-            panel1.setLayout(new GridLayout(2, 3));
-
-            //---- label1 ----
-            label1.setText("Header Entropy Level:");
-            panel1.add(label1);
-
-            //---- headerLevelText ----
-            headerLevelText.setColumns(3);
-            panel1.add(headerLevelText);
-
-            //---- headerSetLevel ----
-            headerSetLevel.setText("Set");
-            headerSetLevel.addActionListener(e -> headerSetLevelActionPerformed(e));
-            panel1.add(headerSetLevel);
+            panel1.setLayout(new GridLayout(1, 3));
 
             //---- label2 ----
             label2.setText("Parameter Entropy Level");
@@ -90,9 +68,6 @@ public class BurpCleanerPanel extends JPanel {
     // Generated using JFormDesigner Evaluation license - unknown
     private JTabbedPane cleanerTabPane;
     private JPanel panel1;
-    private JLabel label1;
-    private JTextField headerLevelText;
-    private JButton headerSetLevel;
     private JLabel label2;
     private JTextField parameterLevelText;
     private JButton parameterSetLevel;
